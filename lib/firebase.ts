8f4dcdc504355d5607116f534a6b78ae67898736
dwyfrequency <jackdwyer@google.com>
2023-03-13 19:37:07 -0400

    Add firebaseConfig details for gh action

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,6 +1,6 @@
 // Import the functions from the Firebase SDK
-import { getApp, getApps, initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getApp, getApps, initializeApp } from 'firebase/app';
+import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 import {
   collection,
   getDocs,
@@ -8,17 +8,17 @@ import {
   limit,
   query,
   where,
-} from "firebase/firestore";
-import { getStorage } from "firebase/storage";
+} from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
 
 // App's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_APP_ID,
+  apiKey: 'AIzaSyAMiCFxC_26ex8ZD3q0zaR7KWMN8tYMZC0',
+  authDomain: 'next-platform-mvp-tests.firebaseapp.com',
+  projectId: 'next-platform-mvp-tests',
+  storageBucket: 'next-platform-mvp-tests.appspot.com',
+  messagingSenderId: '753273276081',
+  appId: '1:753273276081:web:e035a80beaea16c3ef8490',
 };
 
 // Initialize Firebase
@@ -34,8 +34,8 @@ export const googleAuthProvider = new GoogleAuthProvider();
  * @param  {string} username
  */
 export async function getUserWithUsername(username: string) {
-  const usersRef = collection(firestore, "users");
-  const usersFilter = where("username", "==", username);
+  const usersRef = collection(firestore, 'users');
+  const usersFilter = where('username', '==', username);
   const queryUsers = query(usersRef, usersFilter, limit(1));
   const userDocs = await getDocs(queryUsers);
   return userDocs.docs.at(0);
